854f091ba7e54f70123005a43aed0283b7dd2f1c
huyhb <huyhb@unit.com.vn>
2021-06-21 13:24:07 +0700

    xxx

---
+++ app/src/main/java/com/huyhuynh/mypokedex/test/loginwazure/AzureLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.huyhuynh.mypokedex.test.loginwazure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.huyhuynh.mypokedex.R
+
+class AzureLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_azure_login)
+    }
+}
