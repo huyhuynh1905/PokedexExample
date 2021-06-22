bb92d23d572201069e346b172f1eb619d84c86e2
huyhb <huyhb@unit.com.vn>
2021-06-22 11:50:45 +0700

    update comment

--- app/src/main/java/com/huyhuynh/mypokedex/test/loginwazure/AzureLoginActivity.kt
+++ app/src/main/java/com/huyhuynh/mypokedex/test/loginwazure/AzureLoginActivity.kt
@@ -116,12 +116,13 @@ class AzureLoginActivity : AppCompatActivity() {
     }
 
     private fun callGraphAPI(authenticationResult: IAuthenticationResult) {
-        val accessToken: String = authenticationResult.getAccessToken()
+        val accessToken: String = authenticationResult.accessToken
         val graphClient: IGraphServiceClient = GraphServiceClient
             .builder()
             .authenticationProvider { request ->
                 Utils.log("Authenticating","request: ${request.requestUrl}")
                 Utils.log("Authenticating","Bearer: $accessToken")
+                Utils.log("Authenticating","Bearer replace: ${accessToken.replace("/",".")}")
                 request.addHeader("Authorization", "Bearer $accessToken")
             }
             .buildClient()
